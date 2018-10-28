5ebb93f7630cd2c2b21c656f5326481188cdd837
Matt Harrison <matthewdh@gmail.com>
2018-10-28 11:32:24 -0400

    added reconsider modes. moved filters to overlay. added instructions overlay. added reset.

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,102 @@
   <div id="app">
     <header id="header" class="m10">
       <h1 class="mb5">Movie Swiper</h1>
-      <p class="mb10">There are too many movies to watch. Narrow down which movie to watch right now.</p>
-      <i
-      v-if="!showFilters"
-      v-on:click="showFilters = !showFilters"
-      class="fas fa-bars fs20"></i>
-      <div
-      id="filters"
-      v-if="showFilters"
-      class="flex flexWrap">
+      <p class="mb10">There are too many movies to watch. Narrow down which movie to watch.</p>
+      <p class="mb10">{{ mediaTotal }} movies to swipe.</p>
+      <p class="mb10">{{ mediaYes.length }} swiped right.</p>
+      <p class="mb10">{{ mediaNo.length }} swiped left.</p>
+      <p 
+      v-if="mediaUndecided.length === 0 && mediaYes.length === 1"
+      class="mb10">Enjoy your movie: {{ mediaYes[0].original_title }}</p>
+      <div id="buttons" class="flex">
         <i
-        v-on:click="showFilters = false;"
-        class="mr10 fas fa-times-circle fs20"></i>
+        v-on:click.prevent="showInstructions = true;"
+        class="mr10 fas fa-chalkboard-teacher fs20 csrPointer"></i>
+        <i
+        v-on:click.prevent="showFilters = !showFilters"
+        class="mr10 fas fa-filter fs20 csrPointer"></i>
+        <i
+        v-on:click.prevent="reviewMediaYes"
+        class="mr10 fas fa-thumbs-up fs20 csrPointer"></i>
+        <i
+        v-on:click.prevent="reviewMediaUndecided"
+        class="mr10 fas fa-question-circle fs20 csrPointer"></i>
+        <i
+        v-on:click.prevent="reviewMediaNo"
+        class="mr10 fas fa-thumbs-down fs20 csrPointer"></i>
+        <i 
+        v-on:click.prevent="resetAll"
+        class="fas fa-trash fs20 csrPointer"></i>
+      </div>
+    </header>
+    <div
+    id="swipeContainer"
+    class="mxAuto mb10 posRelative overflowHidden">
+      <div
+      id="slides"
+      v-if="currentMediaList.length > 0 && currentMediaList[currentIndex]"
+      v-bind:style="{ left: slidesX + 'px' }"
+      class="flex posAbsolute">
+        <div
+        id="preserve"
+        class="flex justifyCenter alignCenter bgGreen posterSize">
+          <i class="txtGreen fas fa-thumbs-up fs165"></i>
+        </div>
+        <div
+        id="posterContainer"
+        v-on:mousedown.prevent="mouseDown"
+        v-on:touchstart.prevent="touchStart"
+        class="posRelative csrPointer">
+          <img
+          id="poster"
+          v-bind:src="`https://image.tmdb.org/t/p/w500${currentMediaList[currentIndex].poster_path}`"/>
+          <div
+          v-if="currentMediaList === mediaYes"
+          class="flex justifyCenter alignCenter posterSize">
+            <div class="posAbsolute top0 posterSize bgBlack opac50"></div>
+            <div class="posAbsolute top0 flex justifyCenter alignCenter posterSize">
+              <i class="fas fa-thumbs-up fs165 txtGreen"></i>
+            </div>
+          </div>
+          <div
+          v-if="currentMediaList === mediaNo"
+          class="flex justifyCenter alignCenter posterSize">
+            <div class="posAbsolute top0 posterSize bgBlack opac50"></div>
+            <div class="posAbsolute top0 flex justifyCenter alignCenter posterSize">
+              <i class="fas fa-thumbs-down fs165 txtRed"></i>
+            </div>
+          </div>
+        </div>
+        <div
+        id="dismiss"
+        class="flex justifyCenter alignCenter bgRed posterSize">
+          <i class="txtRed fas fa-thumbs-down fs165"></i>
+        </div>
+      </div>
+      <img
+      v-if="mediaUndecided.length === 0 && mediaYes.length === 1"
+      v-on:mousedown.prevent.prevent=""
+      v-bind:src="`https://image.tmdb.org/t/p/w500${mediaYes[0].poster_path}`"
+      class="posterSize"/>
+    </div>
+    <Menu 
+    id="instructions" 
+    v-bind:open="showInstructions" 
+    v-on:close="showInstructions = false;"
+    class="bgWhite">
+      <p>Swipe right to keep a movie in consideration.</p>
+      <p>Swipe left to dismiss.</p>
+      <p>Keep swiping until you've found the movie to watch.</p>
+      <p>Use filters to focus your search.</p>
+    </Menu>
+    <Menu 
+    id="filters" 
+    v-bind:open="showFilters" 
+    v-on:close="updateFilters"
+    class="bgWhite">
+      <div class="flex flexWrap mt10">
         <select
         v-model="params.primary_release_year"
-        v-on:change="updateFilters()"
         class="mr10">
           <option value="">Any Year</option>
           <option
@@ -26,7 +107,6 @@
         </select>
         <select
         v-model="params.with_genres"
-        v-on:change="updateFilters()"
         class="mr10">
           <option value="">Any Genre</option>
           <option
@@ -36,7 +116,6 @@
         </select>
         <select
         v-model="params.certification"
-        v-on:change="updateFilters()"
         class="mr10">
           <option value="">Any Rating</option>
           <option
@@ -45,7 +124,7 @@
           v-bind:value="rating">{{ rating }}</option>
         </select>
         <select
-        v-on:change="updateStarsMinimum"
+        v-on:change.prevent="updateStarsMinimum"
         class="mr10">
           <option value="">Any Minimum Stars</option>
           <option
@@ -54,7 +133,7 @@
           v-bind:value="review.min">{{ review.label }}</option>
         </select>
         <select
-        v-on:change="updateStarsExact"
+        v-on:change.prevent="updateStarsExact"
         class="mr10">
           <option value="">Any Exact Stars</option>
           <option
@@ -64,60 +143,45 @@
           v-bind:data-min="review.min">{{ review.label }}</option>
         </select>
       </div>
-    </header>
-    <div
-    id="swipeContainer"
-    class="mxAuto mb10 posRelative overflowHidden">
-      <div
-      id="slides"
-      v-bind:style="{ left: slidesX + 'px' }"
-      class="flex posAbsolute">
-        <div
-        id="preserve"
-        v-bind:style="{ width: slideWidth + 'px' }"
-        class="flex justifyCenter alignCenter bgGreen">
-          <i class="txtGreen fas fa-thumbs-up"></i>
-        </div>
-        <div
-        id="posterContainer"
-        v-on:mousedown="mouseDown"
-        v-on:touchstart="touchStart">
-          <img
-          id="poster"
-          v-if="media.length > 0 && media[currentIndex]"
-          v-bind:src="`https://image.tmdb.org/t/p/original${media[currentIndex].poster_path}`"
-          class="csrPointer"/>
-        </div>
-        <div
-        id="dismiss"
-        v-bind:style="{ width: slideWidth + 'px' }"
-        class="flex justifyCenter alignCenter bgRed">
-          <i class="txtRed fas fa-thumbs-down"></i>
-        </div>
-      </div>
-    </div>
-    <div id="debug" class="m10 overflowHidden">
-      <p>DEBUG</p>
-      <p>index: {{ currentIndex }}/{{ media.length }}/{{ mediaTotal }}</p>
-      <p>page: {{ params.page }}/{{ pagesTotal }}</p>
-      <pre>preservedMedia: {{ preservedMedia }}</pre>
-      <pre>dismissedMedia: {{ dismissedMedia }}</pre>
+    </Menu>
+    <div id="debug" class="flex">
+      <pre>
+        <p>current ({{ currentMediaList.length }}):</p>
+        {{ currentMediaList }}
+      </pre>
+      <pre>
+        <p>undecided ({{ mediaUndecided.length }}):</p>
+        {{ mediaUndecided }}
+      </pre>
+      <pre>
+        <p>yes ({{ mediaYes.length }}):</p>
+        {{ mediaYes }}
+      </pre>
+      <pre>
+        <p>no ({{ mediaNo.length }}):</p>
+        {{ mediaNo }}
+      </pre>
     </div>
   </div>
 </template>
 
 <script>
   import api from '@/api';
+  import Menu from '@/components/Menu.vue';
 
   export default {
     name: 'app',
+    components: {
+      Menu
+    },
     data() {
       return {
         currentIndex: 0,
         genres: [],
-        media: [],
-        dismissedMedia: [],
-        preservedMedia: [],
+        mediaUndecided: [],
+        mediaYes: [],
+        mediaNo: [],
+        currentMediaList: [],
         mediaTotal: null,
         pagesTotal: null,
         params: {
@@ -151,7 +215,7 @@
           'with_runtime.lte': null,
           with_original_language: null,
           without_keywords: null,
-          year: null,
+          year: null
         },
         ratings: [
           'G',
@@ -197,7 +261,8 @@
         mousedownX: null,
         slidesX: null,
         slideWidth: null,
-        showFilters: false
+        showFilters: false,
+        showInstructions: false
       };
     },
     computed: {
@@ -213,6 +278,41 @@
       }
     },
     methods: {
+      resetParams() {
+        this.params = {
+          certification_country: 'US',
+          certification: '',
+          certification_lte: null,
+          include_adult: false,
+          include_video: false,
+          language: 'en-US',
+          page: 0,
+          'primary_release_date.gte': null,
+          'primary_release_date.lte': null,
+          primary_release_year: '',
+          region: 'US',
+          'release_date.gte': null,
+          'release_date.lte': null,
+          sort_by: null,
+          'vote_average.gte': null,
+          'vote_average.lte': null,
+          'vote_count.gte': null,
+          'vote_count.lte': null,
+          with_cast: null,
+          with_companies: null,
+          with_crew: null,
+          with_genres: '',
+          with_keywords: null,
+          without_genres: null,
+          with_people: null,
+          with_release_type: null,
+          'with_runtime.gte': null,
+          'with_runtime.lte': null,
+          with_original_language: null,
+          without_keywords: null,
+          year: null
+        };
+      },
       getGenres() {
         api.getGenres().then((response) => {
           this.genres = response.data.genres;
@@ -221,51 +321,78 @@
       getMovies() {
         this.params.page++;
         api.getMovies(this.params).then((response) => {
-          this.mediaTotal = response.data.total_results;
+          this.mediaTotal = (this.mediaTotal) ? this.mediaTotal : response.data.total_results;
           this.pagesTotal = response.data.total_pages;
 
           response.data.results.forEach(movie => {
-            this.media.push(movie);
+            this.mediaUndecided.push(movie);
           });
+          
+          this.currentMediaList = this.mediaUndecided;
         });
       },
-      dismissMedia() {
-        this.dismissedMedia.push(this.media[this.currentIndex]);
-        this.media.splice(this.currentIndex, 1);
-        this.mediaTotal--;
-
-        if (this.currentIndex >= this.media.length) {
-          if (this.currentIndex >= this.mediaTotal) {
-            this.currentIndex = 0;
+      checkCurrentList() {
+        if (this.currentMediaList.length === 0) {
+          if (this.currentMediaList === this.mediaYes) {
+            if (this.mediaUndecided.length > 0) {
+              this.reviewMediaUndecided();
+            }
+          } else if (this.currentMediaList === this.mediaNo) {
+            if (this.mediaUndecided.length > 0) {
+              this.reviewMediaUndecided();
+            } else {
+              this.reviewMediaYes();
+            }
           } else {
             if (this.params.page < this.pagesTotal) {
               this.getMovies();
             } else {
-              this.currentIndex = 0;
+              if (this.mediaYes.length > 0) {
+                this.reviewMediaYes();
+              }
             }
           }
         }
+      },
+      dismissMedia() {
+        if (this.currentMediaList === this.mediaYes) {
+          this.mediaNo.push(this.currentMediaList[this.currentIndex]);
+          this.currentMediaList.splice(this.currentIndex, 1);
+        } else if (this.currentMediaList === this.mediaNo) {
+          this.currentIndex++;
+        } else {
+          this.mediaNo.push(this.currentMediaList[this.currentIndex]);
+          this.currentMediaList.splice(this.currentIndex, 1);
+          this.mediaTotal--;
+        }
+        
+        this.checkCurrentList();
 
         setTimeout(this.showNextPoster, 500);
       },
       preserveMedia() {
-        this.preservedMedia.push(this.media[this.currentIndex]);
-        this.currentIndex++;
-
-        if (this.currentIndex === this.media.length) {
-          if (this.params.page < this.pagesTotal) {
-            this.getMovies();
-          } else {
-            this.currentIndex = 0;
-          }
+        if (this.currentMediaList === this.mediaYes) {
+          this.currentIndex++;
+        } else if (this.currentMediaList === this.mediaNo) {
+          this.mediaYes.unshift(this.currentMediaList[this.currentIndex]);
+          this.currentMediaList.splice(this.currentIndex, 1);
+        } else {
+          this.mediaYes.push(this.currentMediaList[this.currentIndex]);
+          this.currentMediaList.splice(this.currentIndex, 1);
+          this.mediaTotal--;
         }
+        
+        this.checkCurrentList();
 
         setTimeout(this.showNextPoster, 500);
       },
       updateFilters() {
+        this.showFilters = false;
         this.currentIndex = 0;
         this.params.page = 0;
-        this.media = [];
+        this.mediaUndecided = [];
+        this.mediaTotal = null;
+        
         this.getMovies();
       },
       updateStarsExact(event) {
@@ -283,11 +410,7 @@
         this.updateFilters();
       },
       mouseDown(event) {
-        if (event.type == 'mousedown') {
-          event.preventDefault();
-        }
-
-        if (this.media.length > 1) {
+        if (this.mediaUndecided.length > 0 || this.mediaYes.length > 0) {
           this.mousedownX = event.clientX;
           this.offset = this.slideWidth + event.clientX - this.containerX;
         }
@@ -323,26 +446,64 @@
         }
       },
       touchStart(event) {
-        event.preventDefault();
         this.mouseDown(event.touches[0]);
       },
       touchMove(event) {
-        event.preventDefault();
         this.mouseMove(event.changedTouches[0]);
       },
       touchEnd(event) {
-        event.preventDefault();
         this.mouseUp(event.changedTouches[0]);
       },
       showNextPoster() {
         this.slidesX = 0 - this.slideWidth;
+      },
+      resetAll() {
+        let confirmation = confirm('Are you sure you want to permanently clear your selections and filters?');
+        
+        if (confirmation) {
+          this.currentIndex = 0;
+          this.mediaUndecided = [];
+          this.mediaYes = [];
+          this.mediaNo = [];
+          
+          this.resetParams();
+          this.getMovies();
+        }
+      },
+      reviewMediaYes() {
+        if (this.mediaYes.length > 0) {
+          this.currentMediaList = this.mediaYes;
+          this.currentIndex = 0;
+        } else {
+          alert('There are no approved movies to dismiss. Swipe right to add movies to consider.');
+          this.reviewMediaUndecided();
+        }
+      },
+      reviewMediaNo() {
+        if (this.mediaNo.length > 0) {
+          this.currentMediaList = this.mediaNo;
+          this.currentIndex = 0;
+        } else {
+          alert('There are no dismissed movies to reconsider. Swipe left to dismiss a movie.');
+          this.reviewMediaUndecided();
+        }
+      },
+      reviewMediaUndecided() {
+        if (this.mediaUndecided.length > 0) {
+          this.currentMediaList = this.mediaUndecided;
+          this.currentIndex = 0;
+        } else {
+          alert("All movies matching your filters have been swiped. Now it's time to make some tough choices. Swipe left to dismiss.");
+          this.reviewMediaYes();
+        }
       }
     },
     mounted() {
       this.containerX = document.getElementById('swipeContainer').getBoundingClientRect().left;
-      this.slideWidth = document.getElementById('posterContainer').getBoundingClientRect().width;
+      this.slideWidth = 200;//document.getElementById('posterContainer').getBoundingClientRect().width;
       this.slidesX = 0 - this.slideWidth;
 
+      this.resetParams();
       this.getGenres();
       this.getMovies();
 
@@ -355,18 +516,12 @@
 </script>
 
 <style scoped>
-  #swipeContainer, #posterContainer, #poster {
+  #swipeContainer, #posterContainer, #poster, .posterSize {
     width: 200px;
     height: 300px;
   }
-  #dismiss, #preserve {
-    font-size: 165px;
+  pre {
+    width: 25%;
+    overflow: hidden;
   }
-  #debug {
-    width: calc(100vw - 50px);
-  }
-  .fa-thumbs-down {
-    transform: scaleX(-1);
-  }
-  .fs20 {font-size: 20px;}
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div 
+  v-if="open"
+  class="bsBorder p20 posAbsolute menu">
+    <div class="flex flexEnd">
+      <i
+      v-on:click="$emit('close')"
+      class="fas fa-times-circle fs20 csrPointer"></i>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    open: Boolean,
+    default: false
+  }
+}
+</script>
+
+<style scoped>
+  .menu {
+    top:  0;
+    left: 0;
+    width:  100vw;
+    height: 100vh;
+  }
+</style>
